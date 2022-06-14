46f159a85ee270aa4ea3332ef9a5da376d1a8322
Creat2die <Jevgenij.s@inbox.lt>
2022-06-14 10:44:10 +0300

    for

--- js/for.js
+++ js/for.js
@@ -30,3 +30,43 @@ for (let i = 0; i< word.length; i++){
     const letter = word[i];
     console.log(i, ')', letter);
 }
+
+console.clear();
+
+for (let i = 10; i > 0 ; i--){
+    console.log(i);
+}
+
+console.log('--------------');
+for (let i = 10; i > 0 ; i--){
+    console.log(i);
+    if (i === 5 ) {
+        break;
+    }
+}
+console.log('--------------');
+
+const money = [5, 10, 20, 50, 100, 5, 10, 20];
+const minTarget = 80;
+const hand = [];
+let handTotal = 0;
+
+for (let i = 0; i < money.length; i++) {
+    const value = money[i];
+    handTotal += value;
+    hand.push(value)
+    console.log(` ${i}) ${value} -> total: ${handTotal};`, hand);
+    if (handTotal >= minTarget) {
+        break;
+    }
+}
+const graza = handTotal - minTarget;
+if(graza > 0) {
+    console.log(`Graza: ${graza}`);
+    console.log(' duotos kupiuros', hand);
+} else if (graza === 0 ){
+    console.log('duota lygiai kiek reikia');
+    console.log('duotos kupiuros', hand);
+} else {
+    console.log(`tu skoloje dar: ${graza * -1}`);
+}
