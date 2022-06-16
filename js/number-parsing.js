a8516233b88378cfcc3c00cf8f7ac340f81c96ed
Creat2die <Jevgenij.s@inbox.lt>
2022-06-16 11:08:13 +0300

    number import

--- js/number-parsing.js
+++ js/number-parsing.js
@@ -1,14 +1,11 @@
 console.clear();
 
-const n1 = 5;
-const n2 = '7a';
-
 function suma(a, b){
     const initialAsize = ('' + a).length;
     const initialBsize = ('' + b).length;
 
-    a  = parseInt(a);
-    b  = parseInt(b);
+    a  = parseFloat(a);
+    b  = parseFloat(b);
 
     const afterAsize = ('' +a).length;
     const afterBsize = ('' +b).length;
@@ -16,12 +13,10 @@ function suma(a, b){
     if(isNaN(a) || initialAsize !== afterAsize){
             return 'Error: Pirmas parametras ne tikras skaicius';
     }
-    if(typeof b !== 'number' || initialBsize !==afterBsize){
+    if(isNaN(b) || initialBsize !==afterBsize){
         return 'Error: Antras parametras ne tikras skaicius';
     }
     return a + b;
 }
 
-const ats = suma(n1,n2);
-console.log(ats);
-
+module.exports = suma;
